79097f4d8ed3a3daa0877df1b0dbb6775226e41d
jasenmichael <jasen@ready-builder.com>
2019-06-17 19:19:26 -0500

    implement statging deploy script

--- pages/events.vue
+++ pages/events.vue
@@ -19,9 +19,25 @@ export default {
     };
   },
   async fetch({ store }) {
+    // if (store.state.events.list.length == 0) {
+    //   await store.dispatch("events/get");
+
+
+  // async asyncData({ store }) {
     if (store.state.events.list.length == 0) {
       await store.dispatch("events/get");
     }
+    if (store.state.events.categories.length == 0) {
+      await store.dispatch("events/getCategories");
+    }
+    if (store.state.events.subcategories.length == 0) {
+      await store.dispatch("events/getSubcategories");
+    }
+  // },
+
+
+
+    // }
   }
 };
 </script>
